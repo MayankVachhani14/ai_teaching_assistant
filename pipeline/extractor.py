2b6cc3726d7593c84fd2d837b008dbc67998987a
Your Name <your@email.comclear>
2026-03-19 23:58:32 +0530

    Fixed audio file saving to data folder

--- pipeline/extractor.py
+++ pipeline/extractor.py
@@ -1,20 +1,16 @@
 import moviepy.editor as mp
-import os  
+import os
 
 def extract_audio(video_path):
-    
-    # we get the name of the video file without the extension to use it for naming the audio file
+
+    # Make sure data folder exists
+    os.makedirs("data", exist_ok=True)
+
     video_name = os.path.splitext(os.path.basename(video_path))[0]
+    audio_path = os.path.join("data", f"{video_name}.wav")
 
-    # here we save the audio file in the data folder with the same name as the video but with .wav extension
-    audio_path = f"data{video_name}.wav"
-
-    # we use moviepy to load the video file and extract the audio, then we save it as a .wav file
     video = mp.VideoFileClip(video_path)
-
-    # we write the audio to the specified path
     video.audio.write_audiofile(audio_path)
-
     video.close()
 
     return audio_path
